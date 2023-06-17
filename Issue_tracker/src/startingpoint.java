ebfa0663833f55a34c5b50edab525a674a2111de
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-06-17 14:28:00 -0500

    small mistake

--- Issue_tracker/src/startingpoint.java
+++ Issue_tracker/src/startingpoint.java
@@ -2,7 +2,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class startingpoint {
-    void startingpoint() {
+    public startingpoint() {
 
     }
 
