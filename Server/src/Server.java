916d3a96d4daf9e6b7f4625778428c5f0caff5ef
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-06-25 20:50:47 -0500

    Changes to be committed: 	new file:   Customer_portal/.vscode/settings.json 	new file:   Customer_portal/README.md 	new file:   Customer_portal/src/App.java 	new file:   Employee_portal/.vscode/settings.json 	new file:   Employee_portal/README.md 	new file:   Employee_portal/src/App.java 	new file:   Server/.vscode/settings.json 	new file:   Server/README.md 	new file:   Server/src/Server.java
    head hurty

---
+++ Server/src/Server.java
@@ -0,0 +1,7 @@
+//https://www.javatpoint.com/steps-to-connect-to-the-database-in-java
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
