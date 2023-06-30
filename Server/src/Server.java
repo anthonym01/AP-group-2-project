2ccbc1468e18984eb2f56b0a6a5529722b11ad65
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-06-30 00:04:58 -0500

    database

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,7 +4,8 @@ public class Server {
     public static void main(String[] args) throws Exception {
         db_man database = new db_man();
 
-        database.test();// test database connection
+        //database.test();// test database connection
+        database.test_customer_table();
     }
 
 }
