b93c68790c6851e7310f8bb5327eead094511c8f
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-07-03 22:11:59 -0500

    cleanup

--- Customer_portal/src/App.java
+++ Customer_portal/src/App.java
@@ -1,22 +1,11 @@
-import org.apache.logging.log4j.Logger;
 
 import views.statingpoint;
-import views.tester;
-import java.awt.Component;
 import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import org.apache.logging.log4j.LogManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class App {
-    Logger loggner;// Log4j2
 
     public static void main(String[] args) {
+        // App kickoff point
         JFrame Appview = new statingpoint().getAppview();
         Appview.setSize(1280, 750);
         Appview.setVisible(true);
@@ -24,8 +13,6 @@ public class App {
     }
 
     public App() {
-        loggner = LogManager.getLogger(App.class);
-        loggner.info("Startup");
     }
 
 }
