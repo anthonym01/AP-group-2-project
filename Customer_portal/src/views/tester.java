15a89e0644813c1c8d91b2164afeb2df71cacbc2
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-07-03 21:07:22 -0500

    broken testview

---
+++ Customer_portal/src/views/tester.java
@@ -0,0 +1,38 @@
+package views;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class tester extends JFrame implements ActionListener {
+    JFrame testview;
+
+    public tester(){
+        //create  test view
+        //testview.setSize(1300, 700);
+
+        //testview.setVisible(true);
+        //testview.setLayout(null);
+        //testview.setLocationRelativeTo(null);
+        //testview.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //testview.setSize(1280, 750);
+
+        JLabel placeholder = new JLabel("test change");
+        placeholder.setBounds(90, 15, 100, 30);
+        testview.add(placeholder);
+    }
+
+    public JFrame getviewFrame(){
+        return testview;
+    }    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+    
+}
