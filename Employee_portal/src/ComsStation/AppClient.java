7aef5c8bf25f73d9c346a0ab9f5fab744fbf84de
Samuel A. Matheson <samuelmatheson15@gmail.com>
2023-07-18 19:55:16 -0500

    cleanup

--- Employee_portal/src/ComsStation/AppClient.java
+++ Employee_portal/src/ComsStation/AppClient.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.Socket; 
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
